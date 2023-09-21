d9548d3a0830e4d14fd2eae6d8d74976e9358eb6
DarawanOmar <darait721@gmail.com>
2023-09-21 11:08:25 +0300

    Update Form 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Contact = () => {
+  const[name,setName] = useState('')
+  const[email,setEmail] = useState('')
+  const[messgae,setMessgae] = useState('')
   return (
     <div className='py-16 lg:section' id='contact'>
       <div className='container mx-auto'>
@@ -15,11 +18,11 @@ const Contact = () => {
               </div>
             </motion.div>
             {/* Form */}
-            <motion.form name="contact" method="POST" data-netlify="true" className='flex-1 border-2  rounded-2xl p-6 flex flex-col gap-y-6 pb-24' variants={fadeIn('left',0.3)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.4}}>
+            <motion.form name="contact" method="POST"  className='flex-1 border-2  rounded-2xl p-6 flex flex-col gap-y-6 pb-24' variants={fadeIn('left',0.3)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.4}}>
               <input type='hidden' name='form-name' value='contact'/>
-              <input type='text' name='name' className='w-full bg-transparent py-3 outline-none border-b focus:border-accent  placeholder:text-right placeholder:text-gray-300 transition-all' placeholder='ناو'/>
-              <input type='email' name='email' className='w-full bg-transparent py-3 outline-none border-b focus:border-accent  placeholder:text-right placeholder:text-gray-300 transition-all' placeholder='ئیمەیڵ'/>
-              <textarea name='message' className='resize-none mb-12 w-full bg-transparent pb-12 outline-none border-b focus:border-accent  placeholder:text-right placeholder:text-gray-300 transition-all' placeholder='نامە' ></textarea>
+              <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} className='w-full bg-transparent py-3 outline-none border-b focus:border-accent  placeholder:text-right placeholder:text-gray-300 transition-all' placeholder='ناو'/>
+              <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} className='w-full bg-transparent py-3 outline-none border-b focus:border-accent  placeholder:text-right placeholder:text-gray-300 transition-all' placeholder='ئیمەیڵ'/>
+              <textarea name='message' id='message' value={messgae} onChange={(e) => setMessgae(e.target.value)} className='resize-none mb-12 w-full bg-transparent pb-12 outline-none border-b focus:border-accent  placeholder:text-right placeholder:text-gray-300 transition-all' placeholder='نامە' ></textarea>
               <button type='submit' className='btn btn-lg'>ناردنی نامە</button>
             </motion.form>
         </div>
